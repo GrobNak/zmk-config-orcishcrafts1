7c8b11420d831ab2c258e1eeb06d7d7efb6a5f01
GrobNak <orcishcrafts@proton.me>
2026-06-15 02:22:52 -0700

    Create status_screen.c

---
+++ boards/shields/orcishcrafts1/src/status_screen.c
@@ -0,0 +1,13 @@
+#include <zephyr/kernel.h>
+#include <zmk/display.h>
+#include <lvgl.h>
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen = lv_obj_create(NULL);
+    
+    // Set background color to warm amber orange (Hex: 0xE67E22)
+    lv_obj_set_style_bg_color(screen, lv_color_hex(0xE67E22), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
+    
+    return screen;
+}
