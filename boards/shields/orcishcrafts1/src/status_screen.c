2ee738067ac6c587a5d37a22153096a45561f83c
GrobNak <orcishcrafts@proton.me>
2026-06-17 19:17:08 -0700

    Update status_screen.c

--- boards/shields/orcishcrafts1/src/status_screen.c
+++ boards/shields/orcishcrafts1/src/status_screen.c
@@ -5,7 +5,7 @@
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen = lv_obj_create(NULL);
     
-    lv_obj_set_size(screen, 170, 320);
+    lv_obj_set_size(screen, 172, 320);
     lv_obj_set_pos(screen, 0, 0);
     lv_obj_clear_flag(screen, LV_OBJ_FLAG_SCROLLABLE);
     
