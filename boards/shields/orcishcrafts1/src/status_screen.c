d0eb837c9c9b0beeeb4e839aa32980fc705146e6
GrobNak <orcishcrafts@proton.me>
2026-06-17 18:21:41 -0700

    Update status_screen.c

--- boards/shields/orcishcrafts1/src/status_screen.c
+++ boards/shields/orcishcrafts1/src/status_screen.c
@@ -13,7 +13,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_bg_color(screen, lv_color_hex(0xE67E22), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
     
-    // Force immediate visual cache synchronization 
+    // Force immediate layout synchronization
     lv_obj_invalidate(screen);
     lv_refr_now(NULL);
     
